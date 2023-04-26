dce4150a268f5ec31b260db2c7371a7ab33fa582
picoHz <53327867+picoHz@users.noreply.github.com>
2023-04-26 20:21:49 +0900

    webui: fix certsStore update issue

--- webui/src/stores/certs.js
+++ webui/src/stores/certs.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 
 export const useCertsStore = defineStore('certs', {
-    state: () => ({ list: {} }),
+    state: () => ({ list: [] }),
     actions: {
         update(certs) {
             this.list = certs
