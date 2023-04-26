e3875e4c111cf0caa8d1e8ee29e653ff2db6e3f1
picoHz <53327867+picoHz@users.noreply.github.com>
2023-04-27 00:43:03 +0900

    wip

--- src/keyring/acme.rs
+++ src/keyring/acme.rs
@@ -1,13 +1,16 @@
-use std::fmt;
-
-use instant_acme::{Account, AccountCredentials, NewAccount};
+use instant_acme::{
+    Account, AccountCredentials, AuthorizationStatus, ChallengeType, Identifier, NewAccount,
+    NewOrder, OrderStatus,
+};
 use serde_derive::{Deserialize, Serialize};
+use std::{fmt, time::Duration};
+use tracing::{error, info};
 
 #[derive(Serialize, Deserialize)]
 pub struct AcmeEntry {
     pub id: String,
     pub provider: String,
-    pub identifiers: Vec<String>,
+    pub identifier: String,
     #[serde(
         serialize_with = "serialize_account",
         deserialize_with = "deserialize_account"
@@ -19,13 +22,17 @@ impl fmt::Debug for AcmeEntry {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("AcmeEntry")
             .field("provider", &self.provider)
-            .field("identifiers", &self.identifiers)
+            .field("identifier", &self.identifier)
             .finish()
     }
 }
 
 impl AcmeEntry {
-    pub async fn new(provider: &str, server_url: &str) -> Result<Self, instant_acme::Error> {
+    pub async fn new(
+        provider: &str,
+        server_url: &str,
+        identifier: &str,
+    ) -> Result<Self, instant_acme::Error> {
         let account = Account::create(
             &NewAccount {
                 contact: &[],
@@ -38,9 +45,83 @@ impl AcmeEntry {
         Ok(Self {
             id: cuid2::create_id(),
             provider: provider.to_string(),
-            identifiers: Vec::new(),
+            identifier: identifier.to_string(),
             account,
         })
+    }
+
+    pub async fn update(&mut self) -> Result<(), instant_acme::Error> {
+        let identifier = Identifier::Dns(self.identifier.clone());
+        let mut order = self
+            .account
+            .new_order(&NewOrder {
+                identifiers: &[identifier],
+            })
+            .await
+            .unwrap();
+        let state = order.state();
+        println!("order state: {:#?}", state);
+
+        let authorizations = order.authorizations().await.unwrap();
+        let mut challenges = Vec::with_capacity(authorizations.len());
+        for authz in &authorizations {
+            match authz.status {
+                AuthorizationStatus::Pending => {}
+                AuthorizationStatus::Valid => continue,
+                _ => todo!(),
+            }
+
+            // We'll use the DNS challenges for this example, but you could
+            // pick something else to use here.
+
+            let challenge = authz
+                .challenges
+                .iter()
+                .find(|c| c.r#type == ChallengeType::Dns01)
+                .ok_or_else(|| anyhow::anyhow!("no dns01 challenge found"))
+                .unwrap();
+
+            let Identifier::Dns(identifier) = &authz.identifier;
+
+            println!("Please set the following DNS record then press any key:");
+            println!(
+                "_acme-challenge.{} IN TXT {}",
+                identifier,
+                order.key_authorization(challenge).dns_value()
+            );
+            // io::stdin().read_line(&mut String::new()).unwrap();
+
+            challenges.push((identifier, &challenge.url));
+        }
+
+        for (_, url) in &challenges {
+            order.set_challenge_ready(url).await.unwrap();
+        }
+
+        let mut tries = 1u8;
+        let mut delay = Duration::from_millis(250);
+        loop {
+            tokio::time::sleep(delay).await;
+            let state = order.refresh().await.unwrap();
+            if let OrderStatus::Ready | OrderStatus::Invalid = state.status {
+                info!("order state: {:#?}", state);
+                break;
+            }
+
+            delay *= 2;
+            tries += 1;
+            match tries < 5 {
+                true => info!(?state, tries, "order is not ready, waiting {delay:?}"),
+                false => {
+                    error!(?state, tries, "order is not ready");
+                }
+            }
+        }
+
+        let state = order.state();
+        println!("order state: {:#?}", state);
+
+        Ok(())
     }
 
     pub fn id(&self) -> &str {
