c79f511f09bcca1dd022f0379e93b46b72cd5324
picoHz <53327867+picoHz@users.noreply.github.com>
2023-05-01 00:14:55 +0900

    webui: always use dark mode

--- webui/src/plugins/vuetify.js
+++ webui/src/plugins/vuetify.js
@@ -11,12 +11,10 @@ import 'vuetify/styles'
 // Composables
 import { createVuetify } from 'vuetify'
 
-const darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: darkMode ? 'dark' : 'light',
+    defaultTheme: 'dark',
     themes: {},
   },
 })
