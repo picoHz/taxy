9ce8f28b1ee1a27f558bb604ab41c137c33116d2
picoHz <53327867+picoHz@users.noreply.github.com>
2023-05-07 15:51:41 +0900

    wip

---
+++ src/proxy/http.rs
@@ -0,0 +1,307 @@
+use super::{tls::TlsTermination, PortContextEvent, PortStatus, SocketState};
+use crate::{
+    config::{port::PortEntry, AppConfig},
+    error::Error,
+    keyring::Keyring,
+};
+use multiaddr::{Multiaddr, Protocol};
+use std::{
+    net::{IpAddr, SocketAddr},
+    sync::Arc,
+    time::SystemTime,
+};
+use tokio::io::{AsyncRead, AsyncWrite, BufStream};
+use tokio::net::{self, TcpSocket, TcpStream};
+use tokio_rustls::{
+    rustls::{client::ServerName, Certificate, ClientConfig, RootCertStore},
+    TlsAcceptor, TlsConnector,
+};
+use tracing::{debug, error, info, span, warn, Instrument, Level, Span};
+
+#[derive(Debug)]
+pub struct HttpPortContext {
+    pub listen: SocketAddr,
+    servers: Vec<Connection>,
+    status: PortStatus,
+    span: Span,
+    tls_termination: Option<TlsTermination>,
+    tls_client_config: Option<Arc<ClientConfig>>,
+    round_robin_counter: usize,
+}
+
+impl HttpPortContext {
+    pub fn new(port: &PortEntry) -> Result<Self, Error> {
+        let span = span!(Level::INFO, "proxy", resource_id = port.id, listen = ?port.listen);
+        let enter = span.clone();
+        let _enter = enter.enter();
+
+        info!("initializing http proxy");
+
+        let listen = multiaddr_to_tcp(&port.listen)?;
+
+        if port.servers.is_empty() {
+            return Err(Error::EmptyBackendServers);
+        }
+
+        let mut servers = Vec::new();
+        for server in &port.servers {
+            let server = multiaddr_to_host(&server.addr)?;
+            servers.push(server);
+        }
+
+        let mut tls_client_config = None;
+        let use_tls = servers.iter().any(|server| server.tls);
+        if use_tls {
+            let mut root_certs = RootCertStore::empty();
+            match rustls_native_certs::load_native_certs() {
+                Ok(certs) => {
+                    for certs in certs {
+                        if let Err(err) = root_certs.add(&Certificate(certs.0)) {
+                            warn!("failed to add native certs: {err}");
+                        }
+                    }
+                }
+                Err(err) => {
+                    warn!("failed to load native certs: {err}");
+                }
+            }
+            let config = ClientConfig::builder()
+                .with_safe_defaults()
+                .with_root_certificates(root_certs)
+                .with_no_client_auth();
+            tls_client_config = Some(Arc::new(config));
+        }
+
+        let tls_termination = if let Some(tls) = &port.opts.tls_termination {
+            Some(TlsTermination::new(tls)?)
+        } else if port.listen.iter().any(|p| p == Protocol::Tls) {
+            return Err(Error::TlsTerminationConfigMissing);
+        } else {
+            None
+        };
+
+        Ok(Self {
+            listen,
+            servers,
+            status: Default::default(),
+            span,
+            tls_termination,
+            tls_client_config,
+            round_robin_counter: 0,
+        })
+    }
+
+    pub async fn prepare(&mut self, _config: &AppConfig) -> Result<(), Error> {
+        let use_tls = self.servers.iter().any(|server| server.tls);
+        if self.tls_client_config.is_none() && use_tls {
+            let mut root_certs = RootCertStore::empty();
+            if let Ok(certs) =
+                tokio::task::spawn_blocking(rustls_native_certs::load_native_certs).await
+            {
+                match certs {
+                    Ok(certs) => {
+                        for certs in certs {
+                            if let Err(err) = root_certs.add(&Certificate(certs.0)) {
+                                warn!("failed to add native certs: {err}");
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        warn!("failed to load native certs: {err}");
+                    }
+                }
+            }
+            let config = ClientConfig::builder()
+                .with_safe_defaults()
+                .with_root_certificates(root_certs)
+                .with_no_client_auth();
+            self.tls_client_config = Some(Arc::new(config));
+        }
+
+        Ok(())
+    }
+
+    pub async fn setup(&mut self, keyring: &Keyring) -> Result<(), Error> {
+        if let Some(tls) = &mut self.tls_termination {
+            self.status.state.tls = Some(tls.setup(keyring).await);
+        }
+        Ok(())
+    }
+
+    pub async fn refresh(&mut self, certs: &Keyring) -> Result<(), Error> {
+        if let Some(tls) = &mut self.tls_termination {
+            self.status.state.tls = Some(tls.refresh(certs).await);
+        }
+        Ok(())
+    }
+
+    pub fn apply(&mut self, new: Self) {
+        *self = Self {
+            round_robin_counter: self.round_robin_counter,
+            ..new
+        };
+    }
+
+    pub fn event(&mut self, event: PortContextEvent) {
+        match event {
+            PortContextEvent::SocketStateUpadted(state) => {
+                if self.status.state.socket != state {
+                    self.status.started_at = if state == SocketState::Listening {
+                        Some(SystemTime::now())
+                    } else {
+                        None
+                    };
+                }
+                self.status.state.socket = state;
+            }
+        }
+    }
+
+    pub fn status(&self) -> &PortStatus {
+        &self.status
+    }
+
+    pub fn start_proxy(&mut self, stream: BufStream<TcpStream>) {
+        let span = self.span.clone();
+        let conn = self.servers[self.round_robin_counter % self.servers.len()].clone();
+        let tls_client_config = self
+            .tls_client_config
+            .as_ref()
+            .filter(|_| conn.tls)
+            .cloned();
+        let tls_acceptor = self
+            .tls_termination
+            .as_ref()
+            .and_then(|tls| tls.acceptor.clone());
+
+        tokio::spawn(
+            async move {
+                if let Err(err) = start(stream, conn, tls_client_config, tls_acceptor).await {
+                    error!("{err}");
+                }
+            }
+            .instrument(span),
+        );
+        self.round_robin_counter = self.round_robin_counter.wrapping_add(1);
+    }
+}
+
+pub async fn start(
+    stream: BufStream<TcpStream>,
+    conn: Connection,
+    tls_client_config: Option<Arc<ClientConfig>>,
+    tls_acceptor: Option<TlsAcceptor>,
+) -> anyhow::Result<()> {
+    let remote = stream.get_ref().peer_addr()?;
+    let local = stream.get_ref().local_addr()?;
+
+    let host = match conn.name.clone() {
+        ServerName::DnsName(name) => format!("{}:{}", name.as_ref(), conn.port),
+        ServerName::IpAddress(addr) => format!("{}:{}", addr, conn.port),
+        _ => unreachable!(),
+    };
+
+    let resolved = net::lookup_host(&host).await?.next().unwrap();
+    debug!(host, %resolved);
+
+    info!(target: "taxy::access_log", remote = %remote, %local, %resolved);
+
+    let mut stream: Box<dyn IoStream> = Box::new(stream);
+    if let Some(acceptor) = tls_acceptor {
+        debug!(%remote, "server: tls handshake");
+        stream = Box::new(acceptor.accept(stream).await?);
+    }
+
+    let service = hyper::service::service_fn(move |req| {
+        let tls_client_config = tls_client_config.clone();
+        let conn = conn.clone();
+
+        async move {
+            let sock = if resolved.is_ipv4() {
+                TcpSocket::new_v4()
+            } else {
+                TcpSocket::new_v6()
+            }
+            .unwrap();
+
+            let out = sock.connect(resolved).await.unwrap();
+            debug!(%resolved, "connected");
+
+            let mut out: Box<dyn IoStream> = Box::new(out);
+            if let Some(config) = tls_client_config {
+                debug!(%resolved, "client: tls handshake");
+                let tls = TlsConnector::from(config);
+                out = Box::new(tls.connect(conn.name, out).await.unwrap());
+            }
+
+            let (mut sender, conn) = hyper::client::conn::http1::handshake(out).await?;
+            tokio::task::spawn(async move {
+                if let Err(err) = conn.await {
+                    error!("Connection failed: {:?}", err);
+                }
+            });
+
+            sender.send_request(req).await
+        }
+    });
+
+    tokio::task::spawn(async move {
+        if let Err(err) = hyper::server::conn::http1::Builder::new()
+            .serve_connection(stream, service)
+            .await
+        {
+            error!("Failed to serve the connection: {:?}", err);
+        }
+    });
+
+    debug!(%resolved, "eof");
+    Ok(())
+}
+
+fn multiaddr_to_tcp(addr: &Multiaddr) -> Result<SocketAddr, Error> {
+    let stack = addr.iter().collect::<Vec<_>>();
+    match &stack[..] {
+        [Protocol::Ip4(addr), Protocol::Tcp(port), ..] if *port > 0 => {
+            Ok(SocketAddr::new(std::net::IpAddr::V4(*addr), *port))
+        }
+        [Protocol::Ip6(addr), Protocol::Tcp(port), ..] if *port > 0 => {
+            Ok(SocketAddr::new(std::net::IpAddr::V6(*addr), *port))
+        }
+        _ => Err(Error::InvalidListeningAddress { addr: addr.clone() }),
+    }
+}
+
+fn multiaddr_to_host(addr: &Multiaddr) -> Result<Connection, Error> {
+    let stack = addr.iter().collect::<Vec<_>>();
+    let tls = stack.last() == Some(&Protocol::Tls);
+    match stack[..] {
+        [Protocol::Ip4(addr), Protocol::Tcp(port), ..] if port > 0 => Ok(Connection {
+            name: ServerName::IpAddress(IpAddr::V4(addr)),
+            port,
+            tls,
+        }),
+        [Protocol::Ip6(addr), Protocol::Tcp(port), ..] if port > 0 => Ok(Connection {
+            name: ServerName::IpAddress(IpAddr::V6(addr)),
+            port,
+            tls,
+        }),
+        [Protocol::Dns(ref name), Protocol::Tcp(port), ..] if port > 0 => Ok(Connection {
+            name: ServerName::try_from(name.as_ref())
+                .map_err(|_| Error::InvalidServerAddress { addr: addr.clone() })?,
+            port,
+            tls,
+        }),
+        _ => Err(Error::InvalidServerAddress { addr: addr.clone() }),
+    }
+}
+
+trait IoStream: AsyncRead + AsyncWrite + Unpin + Send {}
+
+impl<S> IoStream for S where S: AsyncRead + AsyncWrite + Unpin + Send {}
+
+#[derive(Debug, Clone)]
+pub struct Connection {
+    pub name: ServerName,
+    pub port: u16,
+    pub tls: bool,
+}
