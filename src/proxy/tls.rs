ded8169d2402f4d076895e8936d5491ef2cfe11f
picoHz <53327867+picoHz@users.noreply.github.com>
2023-05-08 22:07:40 +0900

    tls: use sni for certificate resolution

--- src/proxy/tls.rs
+++ src/proxy/tls.rs
@@ -1,3 +1,4 @@
+use crate::keyring::certs::Cert;
 use crate::keyring::subject_name::SubjectName;
 use crate::keyring::Keyring;
 use crate::{config, error::Error};
@@ -5,18 +6,18 @@ use serde_derive::Serialize;
 use std::fmt;
 use std::str::FromStr;
 use std::sync::Arc;
+use tokio_rustls::rustls::server::{ClientHello, ResolvesServerCert};
+use tokio_rustls::rustls::sign::CertifiedKey;
 use tokio_rustls::rustls::ServerConfig;
 use tokio_rustls::TlsAcceptor;
-use tracing::error;
 use utoipa::ToSchema;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, ToSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum TlsState {
     Active,
-    NoValidCertificate,
-    ConfigurationFailed,
 }
+
 pub struct TlsTermination {
     pub server_names: Vec<SubjectName>,
     pub acceptor: Option<TlsAcceptor>,
@@ -44,30 +45,16 @@ impl TlsTermination {
     }
 
     pub async fn setup(&mut self, keyring: &Keyring) -> TlsState {
-        let server_names = self.server_names.clone();
-
-        let cert = if let Some(cert) = keyring.find_server_cert(&server_names) {
-            cert
-        } else {
-            error!(?server_names, "no valid certificate found");
-            return TlsState::NoValidCertificate;
-        };
-
-        let chain = cert.chain.clone();
-        let key = cert.key.clone();
+        let resolver: Arc<dyn ResolvesServerCert> = Arc::new(ServerCertResolver::new(
+            keyring.certs(),
+            self.server_names.clone(),
+            true,
+        ));
 
         let server_config = ServerConfig::builder()
             .with_safe_defaults()
             .with_no_client_auth()
-            .with_single_cert(chain, key);
-
-        let server_config = match server_config {
-            Ok(config) => config,
-            Err(err) => {
-                error!(?err, ?server_names);
-                return TlsState::ConfigurationFailed;
-            }
-        };
+            .with_cert_resolver(resolver);
 
         let server_config = Arc::new(server_config);
         self.acceptor = Some(TlsAcceptor::from(server_config));
@@ -77,5 +64,45 @@ impl TlsTermination {
 
     pub async fn refresh(&mut self, certs: &Keyring) -> TlsState {
         self.setup(certs).await
+    }
+}
+
+pub struct ServerCertResolver {
+    certs: Vec<Arc<Cert>>,
+    default_names: Vec<SubjectName>,
+    sni: bool,
+}
+
+impl ServerCertResolver {
+    pub fn new(certs: Vec<Arc<Cert>>, default_names: Vec<SubjectName>, sni: bool) -> Self {
+        Self {
+            certs,
+            default_names,
+            sni,
+        }
+    }
+}
+
+impl ResolvesServerCert for ServerCertResolver {
+    fn resolve(&self, client_hello: ClientHello) -> Option<Arc<CertifiedKey>> {
+        let sni = client_hello
+            .server_name()
+            .filter(|_| self.sni)
+            .map(|sni| SubjectName::DnsName(sni.into()))
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        let names = if sni.is_empty() {
+            &self.default_names
+        } else {
+            &sni
+        };
+
+        let cert = self
+            .certs
+            .iter()
+            .find(|cert| cert.is_valid() && names.iter().all(|name| cert.has_subject_name(name)))?;
+
+        Some(cert.certified.clone())
     }
 }
