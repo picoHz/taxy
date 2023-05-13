3d653ce7003479d880ef14ed9b25a7c4e54694fd
picoHz <53327867+picoHz@users.noreply.github.com>
2023-05-14 00:16:19 +0900

    wip

--- webui/src/i18n/en_US.js
+++ webui/src/i18n/en_US.js
@@ -67,9 +67,9 @@ export default {
             }
         }
     },
-    _keyring: {
-        keyring: 'Keyring',
-        no_certs: 'No keyring item found.',
+    server_certs: {
+        server_certs: 'Server Certificates',
+        no_certs: 'No certificates found.',
         info: {
             san: 'SAN',
             fingerprint: 'Fingerprint',
@@ -98,30 +98,11 @@ export default {
             rule_key: "Select valid private key.",
             cancel: "Cancel"
         },
-        acme: {
-            acme: "ACME",
-            title: "New ACME Request",
-            provider: "Provider",
-            challenge: "Challenge",
-            email: "Email",
-            domain: "Domain Name",
-            create: "Create",
-            cancel: "Cancel",
-            rule: {
-                hostname_required: 'Provide a valid hostname.',
-            }
-        },
         delete_cert: {
             delete_cert: "Delete Certificate",
             confirm: "Are you sure to delete {id}?",
             delete: "Delete",
             cancel: "Cancel"
-        },
-        delete_acme: {
-            delete_acme: "Delete ACME",
-            confirm: "Are you sure to delete {id}?",
-            cancel: "Cancel",
-            delete: "Delete"
         }
     },
     acme: {
