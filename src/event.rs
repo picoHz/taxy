20b8ade8f1802c8ac8cb4a05565533709b055e98
picoHz <53327867+picoHz@users.noreply.github.com>
2023-05-17 23:50:07 +0900

    ServerEvent::PortTableUpdated: remove source field

--- src/event.rs
+++ src/event.rs
@@ -9,23 +9,10 @@ use utoipa::ToSchema;
 #[derive(Debug, Clone, Serialize, ToSchema)]
 #[serde(rename_all = "snake_case", tag = "event")]
 pub enum ServerEvent {
-    AppConfigUpdated {
-        config: AppConfig,
-        source: Source,
-    },
-    PortTableUpdated {
-        entries: Vec<PortEntry>,
-        source: Source,
-    },
-    PortStatusUpdated {
-        id: String,
-        status: PortStatus,
-    },
-    ServerCertsUpdated {
-        items: Vec<CertInfo>,
-    },
-    AcmeUpdated {
-        items: Vec<AcmeInfo>,
-    },
+    AppConfigUpdated { config: AppConfig, source: Source },
+    PortTableUpdated { entries: Vec<PortEntry> },
+    PortStatusUpdated { id: String, status: PortStatus },
+    ServerCertsUpdated { items: Vec<CertInfo> },
+    AcmeUpdated { items: Vec<AcmeInfo> },
     Shutdown,
 }
