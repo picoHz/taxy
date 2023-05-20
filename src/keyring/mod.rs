169b2a8f2bf809b2c44dff4b22a82d624ef6dbf2
picoHz <53327867+picoHz@users.noreply.github.com>
2023-05-20 17:26:42 +0900

    make system keystore optional and add --no-keystore flag

--- src/keyring/mod.rs
+++ src/keyring/mod.rs
@@ -128,24 +128,37 @@ const APPKEY_KEYRING_SERVICE: &str = "taxy.encryption";
 const APPKEY_KEYRING_USER: &str = "taxy";
 const APPKEY_LENGTH: usize = 32;
 
-pub fn load_appkey() -> anyhow::Result<Zeroizing<Vec<u8>>> {
-    static ENTRY: OnceCell<keyring::Entry> = OnceCell::new();
-    let entry = ENTRY
-        .get_or_try_init(|| keyring::Entry::new(APPKEY_KEYRING_SERVICE, APPKEY_KEYRING_USER))?;
+static ENTRY: OnceCell<Option<keyring::Entry>> = OnceCell::new();
 
-    let password: keyring::Result<Zeroizing<String>> = entry.get_password().map(Zeroizing::new);
+pub fn init_appkey(use_keystore: bool) -> anyhow::Result<()> {
+    ENTRY.get_or_try_init(|| {
+        if use_keystore {
+            keyring::Entry::new(APPKEY_KEYRING_SERVICE, APPKEY_KEYRING_USER)
+                .map(|entry| Some(entry))
+        } else {
+            Ok(None)
+        }
+    })?;
+    Ok(())
+}
 
-    match password {
-        Ok(password) => Ok(Zeroizing::new(
+pub fn load_appkey() -> anyhow::Result<Option<Zeroizing<Vec<u8>>>> {
+    let entry = if let Some(entry) = ENTRY.get().and_then(|entry| entry.as_ref()) {
+        entry
+    } else {
+        return Ok(None);
+    };
+    match entry.get_password() {
+        Ok(password) => Ok(Some(Zeroizing::new(
             general_purpose::STANDARD_NO_PAD.decode::<&str>(&password)?,
-        )),
+        ))),
         Err(keyring::Error::NoEntry) => {
             info!("generating appkey...");
             let mut key = Zeroizing::new(vec![0u8; APPKEY_LENGTH]);
             rand::thread_rng().fill_bytes(key.as_mut());
             let password = Zeroizing::new(general_purpose::STANDARD_NO_PAD.encode(&key));
             entry.set_password(&password)?;
-            Ok(key)
+            Ok(Some(key))
         }
         Err(err) => Err(err.into()),
     }
