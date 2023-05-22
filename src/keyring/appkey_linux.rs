bf1b38479103beba50d68ed3c26c33aa81f8788d
picoHz <53327867+picoHz@users.noreply.github.com>
2023-05-22 23:00:27 +0900

    disable keyring crate on linux

---
+++ src/keyring/appkey_linux.rs
@@ -0,0 +1,15 @@
+#![cfg(target_os = "linux")]
+
+use zeroize::Zeroizing;
+
+pub fn init_appkey(use_keystore: bool) -> anyhow::Result<()> {
+    if use_keystore {
+        Err(anyhow::anyhow!("not implemented"))
+    } else {
+        Ok(())
+    }
+}
+
+pub fn load_appkey() -> anyhow::Result<Option<Zeroizing<Vec<u8>>>> {
+    Ok(None)
+}
--- src/keyring/mod.rs
+++ src/keyring/mod.rs
@@ -3,17 +3,20 @@ use self::{
     certs::{Cert, CertInfo},
 };
 use crate::config::subject_name::SubjectName;
-use base64::{engine::general_purpose, Engine as _};
-use log::info;
-use once_cell::sync::OnceCell;
-use rand::RngCore;
 use serde_derive::Serialize;
 use std::{collections::HashMap, sync::Arc};
 use utoipa::ToSchema;
-use zeroize::Zeroizing;
 
 pub mod acme;
+mod appkey;
+mod appkey_linux;
 pub mod certs;
+
+#[cfg(not(target_os = "linux"))]
+pub use appkey::*;
+
+#[cfg(target_os = "linux")]
+pub use appkey_linux::*;
 
 #[derive(Debug, Default)]
 pub struct Keyring {
@@ -119,44 +122,5 @@ impl Keyring {
             .collect::<Vec<_>>();
         list.sort_unstable_by_key(|cert| cert.id().to_string());
         list
-    }
-}
-
-const APPKEY_KEYRING_SERVICE: &str = "taxy.encryption";
-const APPKEY_KEYRING_USER: &str = "taxy";
-const APPKEY_LENGTH: usize = 32;
-
-static ENTRY: OnceCell<Option<keyring::Entry>> = OnceCell::new();
-
-pub fn init_appkey(use_keystore: bool) -> anyhow::Result<()> {
-    ENTRY.get_or_try_init(|| {
-        if use_keystore {
-            keyring::Entry::new(APPKEY_KEYRING_SERVICE, APPKEY_KEYRING_USER).map(Some)
-        } else {
-            Ok(None)
-        }
-    })?;
-    Ok(())
-}
-
-pub fn load_appkey() -> anyhow::Result<Option<Zeroizing<Vec<u8>>>> {
-    let entry = if let Some(entry) = ENTRY.get().and_then(|entry| entry.as_ref()) {
-        entry
-    } else {
-        return Ok(None);
-    };
-    match entry.get_password() {
-        Ok(password) => Ok(Some(Zeroizing::new(
-            general_purpose::STANDARD_NO_PAD.decode::<&str>(&password)?,
-        ))),
-        Err(keyring::Error::NoEntry) => {
-            info!("generating appkey...");
-            let mut key = Zeroizing::new(vec![0u8; APPKEY_LENGTH]);
-            rand::thread_rng().fill_bytes(key.as_mut());
-            let password = Zeroizing::new(general_purpose::STANDARD_NO_PAD.encode(&key));
-            entry.set_password(&password)?;
-            Ok(Some(key))
-        }
-        Err(err) => Err(err.into()),
     }
 }
