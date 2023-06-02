1f26a3c97e7212bc00be7082885c59f3c93fde3d
picoHz <53327867+picoHz@users.noreply.github.com>
2023-06-02 22:23:14 +0900

    wip

--- taxy-webui/src/login.rs
+++ taxy-webui/src/login.rs
@@ -1,11 +1,41 @@
 use crate::Route;
+use gloo_net::http::Request;
+use serde_derive::{Deserialize, Serialize};
+// use taxy_api::auth::LoginRequest;
 use ybc::TileCtx::{Ancestor, Parent};
 use yew::prelude::*;
 use yew_router::prelude::*;
 
+#[derive(Clone, Serialize, Deserialize)]
+pub struct LoginRequest {
+    pub username: String,
+    pub password: String,
+}
+
 #[function_component(Secure)]
 pub fn secure() -> Html {
     let navigator = use_navigator().unwrap();
+
+    use_effect_with_deps(
+        move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let _login: LoginRequest = Request::post("http://127.0.0.1:46492/api/login")
+                    .json(&LoginRequest {
+                        username: "admin".to_string(),
+                        password: "passw0rd".to_string(),
+                    })
+                    .unwrap()
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+            });
+            || ()
+        },
+        (),
+    );
 
     let onclick = Callback::from(move |_| navigator.push(&Route::Home));
     html! {
