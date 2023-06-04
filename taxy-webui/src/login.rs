8685ad22c8cd2086d280f52b7ceeb32b5e591d2d
picoHz <53327867+picoHz@users.noreply.github.com>
2023-06-04 14:01:47 +0900

    wip

--- taxy-webui/src/login.rs
+++ taxy-webui/src/login.rs
@@ -23,18 +23,44 @@ enum ApiResult<T> {
 pub fn login() -> Html {
     let (_, dispatch) = use_store::<UserSession>();
     let navigator = use_navigator().unwrap();
-    let name = use_state(|| String::new());
+
+    let username = use_state(|| String::new());
+    let password = use_state(|| String::new());
+
+    let oninput_username = Callback::from({
+        let username = username.clone();
+        move |input_event: InputEvent| {
+            let target: HtmlInputElement = input_event
+                .target()
+                .unwrap_throw()
+                .dyn_into()
+                .unwrap_throw();
+            username.set(target.value());
+        }
+    });
+
+    let oninput_password = Callback::from({
+        let password = password.clone();
+        move |input_event: InputEvent| {
+            let target: HtmlInputElement = input_event
+                .target()
+                .unwrap_throw()
+                .dyn_into()
+                .unwrap_throw();
+            password.set(target.value());
+        }
+    });
    
-    let name_clone = name.clone();
     let onclick: Callback<_> = Callback::from(move |_| {
         let navigator = navigator.clone();
         let dispatch = dispatch.clone();
-        let name_clone = name_clone.clone();
+        let username = username.clone();
+        let password = password.clone();
         wasm_bindgen_futures::spawn_local(async move {
             let login: ApiResult<LoginResult> = Request::post(&format!("{API_ENDPOINT}api/login"))
                 .json(&LoginRequest {
-                    username: name_clone.to_string(),
-                    password: "adminx".to_string(),
+                    username: username.to_string(),
+                    password: password.to_string(),
                 })
                 .unwrap()
                 .send()
@@ -57,19 +83,6 @@ pub fn login() -> Html {
             navigator.push(&Route::Login);
         });
     });
-
-    let oninput = Callback::from({
-        let name = name.clone();
-        move |input_event: InputEvent| {
-            let target: HtmlInputElement = input_event
-                .target()
-                .unwrap_throw()
-                .dyn_into()
-                .unwrap_throw();
-            gloo_console::log!(&format!("{:?}", target.value()));
-            name.set(target.value());
-        }
-    });
     
 
     html! {
@@ -80,11 +93,11 @@ pub fn login() -> Html {
                     <ybc::Field>
                         <label class={classes!("label")}>{ "Username" }</label>
                         <div class={classes!("control")}>
-                            <input class="input" type="text" {oninput} />
+                            <input class="input" type="text" oninput={oninput_username} />
                         </div>
                         <label class={classes!("label")}>{ "Password" }</label>
                         <div class={classes!("control")}>
-                            <input class="input" type="password" />
+                            <input class="input" type="password" oninput={oninput_password} />
                         </div>
                         <div class={classes!("control")}>
                             <button class={classes!("button", "is-primary")} {onclick}>{ "Go Home" }</button>
