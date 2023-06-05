f4894f8c67b9c19fe3279d5cb872946042ca0c5b
picoHz <53327867+picoHz@users.noreply.github.com>
2023-06-06 00:30:59 +0900

    wip

--- taxy-webui/src/navbar.rs
+++ taxy-webui/src/navbar.rs
@@ -2,23 +2,63 @@ use crate::pages::Route;
 use yew::prelude::*;
 use yew_router::prelude::*;
 
+struct MenuItem {
+    name: &'static str,
+    icon: &'static str,
+    route: Route,
+}
+
+const ITEMS: &[MenuItem] = {
+    use Route::*;
+    &[MenuItem {
+        name: "Ports",
+        icon: "wifi",
+        route: Ports,
+    }]
+};
+
 #[function_component(Navbar)]
 pub fn navbar() -> Html {
+    let navigator = use_navigator().unwrap();
     let route = use_route::<Route>().unwrap();
     if route == Route::Login {
         return html! {};
     }
+
+    let navigator_cloned = navigator.clone();
+    let onclick = Callback::from(move |e: MouseEvent| {
+        e.prevent_default();
+        navigator_cloned.push(&Route::Home);
+    });
+
     html! {
         <ybc::Navbar
             classes={classes!("is-success")}
             padded=true
             navbrand={html!{
-                <ybc::NavbarItem>
+                <a class="navbar-item" {onclick}>
                     <ybc::Title classes={classes!("has-text-white")} size={ybc::HeaderSize::Is4}>{"Taxy"}</ybc::Title>
-                </ybc::NavbarItem>
+                </a>
             }}
             navstart={html!{
                 <>
+                { ITEMS.into_iter().map(|entry| {
+                    let navigator = navigator.clone();
+                    let onclick = Callback::from(move |e: MouseEvent|  {
+                        e.prevent_default();
+                        navigator.push(&entry.route);
+                    });
+                    html! {
+                        <a class="navbar-item" {onclick}>
+                            <span class="icon-text">
+                                <span class="icon">
+                                    <ion-icon name={entry.icon}></ion-icon>
+                                </span>
+                                <span>{entry.name}</span>
+                            </span>
+                        </a>
+                    }
+                }).collect::<Html>() }
                 <ybc::NavbarItem tag={ybc::NavbarItemTag::A}>
                     <span class="icon-text">
                         <span class="icon">
