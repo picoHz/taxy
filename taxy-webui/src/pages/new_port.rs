63b75b2f5c0139b040f7c4b5d321062b6d4f4269
picoHz <53327867+picoHz@users.noreply.github.com>
2023-06-07 20:16:01 +0900

    wip

--- taxy-webui/src/pages/new_port.rs
+++ taxy-webui/src/pages/new_port.rs
@@ -21,6 +21,54 @@ pub fn new_port() -> Html {
                     <Breadcrumb />
                 </p>
             </ybc::CardHeader>
+
+            <div class="field is-horizontal m-5">
+            <div class="field-label is-normal">
+                <label class="label">{"Department"}</label>
+            </div>
+            <div class="field-body">
+                <div class="field is-narrow">
+                <div class="control">
+                    <div class="select is-fullwidth">
+                    <select>
+                        <option>{"Business development"}</option>
+                        <option>{"Marketing"}</option>
+                        <option>{"Sales"}</option>
+                    </select>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+
+
+            <div class="field is-horizontal m-5">
+            <div class="field-label is-normal">
+              <label class="label">{"From"}</label>
+            </div>
+            <div class="field-body">
+              <div class="field">
+                <p class="control is-expanded has-icons-left">
+                  <input class="input" type="text" placeholder="Name" />
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-user"></i>
+                  </span>
+                </p>
+              </div>
+              <div class="field">
+                <p class="control is-expanded has-icons-left has-icons-right">
+                  <input class="input is-success" type="email" placeholder="Email" value="alex@smith.com" />
+                  <span class="icon is-small is-left">
+                    <i class="fas fa-envelope"></i>
+                  </span>
+                  <span class="icon is-small is-right">
+                    <i class="fas fa-check"></i>
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+
             <ybc::CardFooter>
                 <a class="card-footer-item" onclick={cancel_onclick}>
                     <span class="icon-text">
