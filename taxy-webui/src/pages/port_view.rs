da07d27b8fb8789447839502a388bc1b004a7a0d
picoHz <53327867+picoHz@users.noreply.github.com>
2023-06-08 19:38:50 +0900

    wip

--- taxy-webui/src/pages/port_view.rs
+++ taxy-webui/src/pages/port_view.rs
@@ -1,8 +1,15 @@
 use crate::{
     auth::use_ensure_auth,
     components::{breadcrumb::Breadcrumb, port_config::PortConfig},
+    pages::Route,
+    store::{PortStore, SessionStore},
+    API_ENDPOINT,
 };
+use gloo_net::http::Request;
+use taxy_api::port::PortEntry;
 use yew::prelude::*;
+use yew_router::prelude::*;
+use yewdux::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
@@ -10,8 +17,34 @@ pub struct Props {
 }
 
 #[function_component(PortView)]
-pub fn port_view(_props: &Props) -> Html {
+pub fn port_view(props: &Props) -> Html {
     use_ensure_auth();
+
+    let (ports, _) = use_store::<PortStore>();
+    let port = use_state(|| ports.entries.iter().find(|e| e.id == props.id).cloned());
+    let (session, _) = use_store::<SessionStore>();
+    let token = session.token.clone();
+    let id = props.id.clone();
+    let port_cloned = port.clone();
+    use_effect_with_deps(
+        move |_| {
+            if let Some(token) = token {
+                wasm_bindgen_futures::spawn_local(async move {
+                    if let Ok(entry) = get_port(&token, &id).await {
+                        port_cloned.set(Some(entry));
+                    }
+                });
+            }
+        },
+        session,
+    );
+
+    let navigator = use_navigator().unwrap();
+
+    let navigator_cloned = navigator.clone();
+    let cancel_onclick = Callback::from(move |_| {
+        navigator_cloned.push(&Route::Ports);
+    });
 
     html! {
         <>
@@ -22,28 +55,47 @@ pub fn port_view(_props: &Props) -> Html {
                 </p>
             </ybc::CardHeader>
 
-            <PortConfig />
+            if let Some(entry) = &*port {
+                <PortConfig port={entry.port.clone()} />
 
-            <ybc::CardFooter>
-                <a class="card-footer-item">
-                    <span class="icon-text">
-                    <span class="icon">
-                        <ion-icon name="close"></ion-icon>
-                    </span>
-                    <span>{"Cancel"}</span>
-                    </span>
-                </a>
-                <a class="card-footer-item">
-                    <span class="icon-text">
-                    <span class="icon">
-                        <ion-icon name="checkmark"></ion-icon>
-                    </span>
-                    <span>{"Update"}</span>
-                    </span>
-                </a>
-            </ybc::CardFooter>
+                <ybc::CardFooter>
+                    <a class="card-footer-item" onclick={cancel_onclick}>
+                        <span class="icon-text">
+                        <span class="icon">
+                            <ion-icon name="close"></ion-icon>
+                        </span>
+                        <span>{"Cancel"}</span>
+                        </span>
+                    </a>
+                    <a class="card-footer-item">
+                        <span class="icon-text">
+                        <span class="icon">
+                            <ion-icon name="checkmark"></ion-icon>
+                        </span>
+                        <span>{"Update"}</span>
+                        </span>
+                    </a>
+                </ybc::CardFooter>
+            } else {
+                <ybc::Hero body={
+                    html! {
+                    <p class="title">
+                        {"Not Found"}
+                    </p>
+                    }
+                } />
+            }
 
             </ybc::Card>
         </>
     }
+}
+
+async fn get_port(token: &str, id: &str) -> Result<PortEntry, gloo_net::Error> {
+    Request::get(&format!("{API_ENDPOINT}/ports/{id}"))
+        .header("Authorization", &format!("Bearer {token}"))
+        .send()
+        .await?
+        .json()
+        .await
 }
