a5c4abd6c1c5974bf0f1dc915f1fadd047e6e024
picoHz <53327867+picoHz@users.noreply.github.com>
2023-07-05 18:12:28 +0900

    tests: use mockito for http tests

--- taxy/tests/http_test.rs
+++ taxy/tests/http_test.rs
@@ -1,3 +1,5 @@
+use reqwest::Body;
+use serde_json::json;
 use taxy_api::{
     port::{Port, PortEntry},
     site::{Route, Site, SiteEntry},
@@ -11,6 +13,49 @@ use common::{with_server, TestStorage};
 
 #[tokio::test]
 async fn http_proxy() -> anyhow::Result<()> {
+    let mut server = mockito::Server::new_async().await;
+
+    let mock_get = server
+        .mock("GET", "/hello?world=1")
+        .match_header("via", "taxy")
+        .match_header("forwarded", "for=127.0.0.1")
+        .match_header("x-forwarded-for", mockito::Matcher::Missing)
+        .match_header("x-forwarded-host", mockito::Matcher::Missing)
+        .match_header("x-real-ip", mockito::Matcher::Missing)
+        .match_header("accept-encoding", "gzip, br")
+        .with_body("Hello")
+        .create_async()
+        .await;
+
+    let mock_post = server
+        .mock("POST", "/hello?world=1")
+        .match_header("via", "taxy")
+        .match_header("forwarded", "for=127.0.0.1")
+        .match_header("x-forwarded-for", mockito::Matcher::Missing)
+        .match_header("x-forwarded-host", mockito::Matcher::Missing)
+        .match_header("x-real-ip", mockito::Matcher::Missing)
+        .match_header("accept-encoding", "gzip, br")
+        .match_header("content-type", "application/json")
+        .match_body(mockito::Matcher::Json(json!({"hello": "world"})))
+        .with_body("Hello")
+        .create_async()
+        .await;
+
+    let chunks = vec!["hello"; 1024];
+    let mock_stream = server
+        .mock("POST", "/hello?world=1")
+        .match_header("via", "taxy")
+        .match_header("forwarded", "for=127.0.0.1")
+        .match_header("x-forwarded-for", mockito::Matcher::Missing)
+        .match_header("x-forwarded-host", mockito::Matcher::Missing)
+        .match_header("x-real-ip", mockito::Matcher::Missing)
+        .match_header("accept-encoding", "gzip, br")
+        .match_header("transfer-encoding", "chunked")
+        .match_body(chunks.join("").as_str())
+        .with_body("Hello")
+        .create_async()
+        .await;
+
     let listener = TcpListener::bind("127.0.0.1:52000").await.unwrap();
     let hello = warp::path!("hello").map(|| "Hello".to_string());
     tokio::spawn(warp::serve(hello).run_incoming(TcpListenerStream::new(listener)));
@@ -33,7 +78,7 @@ async fn http_proxy() -> anyhow::Result<()> {
                 routes: vec![Route {
                     path: "/".into(),
                     servers: vec![taxy_api::site::Server {
-                        url: "http://127.0.0.1:52000/".parse().unwrap(),
+                        url: server.url().parse().unwrap(),
                     }],
                 }],
             },
@@ -43,13 +88,49 @@ async fn http_proxy() -> anyhow::Result<()> {
     with_server(config, |_| async move {
         let client = reqwest::Client::builder().build()?;
         let resp = client
-            .get("http://localhost:52001/hello")
+            .get("http://localhost:52001/hello?world=1")
+            .header("x-forwarded-for", "0.0.0.0")
+            .header("x-forwarded-host", "untrusted.example.com")
+            .header("x-real-ip", "0.0.0.0")
             .send()
             .await?
             .text()
             .await?;
         assert_eq!(resp, "Hello");
+
+        let resp = client
+            .post("http://localhost:52001/hello?world=1")
+            .header("x-forwarded-for", "0.0.0.0")
+            .header("x-forwarded-host", "untrusted.example.com")
+            .header("x-real-ip", "0.0.0.0")
+            .json(&json!({"hello": "world"}))
+            .send()
+            .await?
+            .text()
+            .await?;
+        assert_eq!(resp, "Hello");
+
+        let stream = tokio_stream::iter(chunks.into_iter().map(|s| Ok::<_, ::std::io::Error>(s)));
+        let body = Body::wrap_stream(stream);
+
+        let resp = client
+            .post("http://localhost:52001/hello?world=1")
+            .header("x-forwarded-for", "0.0.0.0")
+            .header("x-forwarded-host", "untrusted.example.com")
+            .header("x-real-ip", "0.0.0.0")
+            .body(body)
+            .send()
+            .await?
+            .text()
+            .await?;
+        assert_eq!(resp, "Hello");
+
         Ok(())
     })
-    .await
+    .await?;
+
+    mock_get.assert_async().await;
+    mock_post.assert_async().await;
+    mock_stream.assert_async().await;
+    Ok(())
 }
