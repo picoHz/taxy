4f9c796624141cd43d8feb6e6a84e6ec47c5d5b3
picoHz <53327867+picoHz@users.noreply.github.com>
2024-01-25 00:27:23 +0900

    webui: show certificate expiry

---
+++ taxy-webui/src/format.rs
@@ -0,0 +1,23 @@
+use fancy_duration::FancyDuration;
+use time::{format_description::well_known::Rfc3339, OffsetDateTime};
+use wasm_bindgen::UnwrapThrowExt;
+use web_time::{Duration, SystemTime, UNIX_EPOCH};
+
+pub fn format_expiry(unix_time: i64) -> String {
+    let time = OffsetDateTime::from_unix_timestamp(unix_time).unwrap_throw();
+    let timestamp = time.format(&Rfc3339).unwrap_throw();
+    let date = timestamp.split('T').next().unwrap_throw().to_string();
+
+    let time = UNIX_EPOCH
+        .checked_add(Duration::from_secs(unix_time as u64))
+        .unwrap_throw();
+
+    let duration = if let Ok(duration) = time.duration_since(SystemTime::now()) {
+        let duration = FancyDuration::new(duration).to_string();
+        duration.split(' ').next().unwrap_throw().to_string()
+    } else {
+        "Expired".to_string()
+    };
+
+    format!("{} ({})", date, duration)
+}
