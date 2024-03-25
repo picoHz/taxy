93aa5e90d726a63079eb21f79174e345ebef3fc7
picoHz <53327867+picoHz@users.noreply.github.com>
2024-03-25 20:00:24 +0900

    webui: fix human-friendly duration formatting

--- taxy-webui/src/format.rs
+++ taxy-webui/src/format.rs
@@ -1,9 +1,9 @@
-use fancy_duration::FancyDuration;
+use fancy_duration::{DurationPart, FancyDuration};
 use time::{format_description::well_known::Rfc3339, OffsetDateTime};
 use wasm_bindgen::UnwrapThrowExt;
 use web_time::{Duration, SystemTime, UNIX_EPOCH};
 
-pub fn format_time(unix_time: i64) -> String {
+pub fn format_duration(unix_time: i64) -> String {
     let time = OffsetDateTime::from_unix_timestamp(unix_time).unwrap_throw();
     let timestamp = time.format(&Rfc3339).unwrap_throw();
     let date = timestamp.split('T').next().unwrap_throw().to_string();
@@ -13,8 +13,16 @@ pub fn format_time(unix_time: i64) -> String {
         .unwrap_throw();
 
     let duration = if let Ok(duration) = time.duration_since(SystemTime::now()) {
-        let duration = FancyDuration::new(duration).to_string();
-        duration.split(' ').next().unwrap_throw().to_string()
+        FancyDuration::new(duration)
+            .filter(&[
+                DurationPart::Years,
+                DurationPart::Months,
+                DurationPart::Weeks,
+                DurationPart::Days,
+                DurationPart::Hours,
+            ])
+            .truncate(1)
+            .to_string()
     } else {
         "Expired".to_string()
     };
